0569edb2d38d5289f1a2ad6432f2709bc82fab4e
Armira5 <93438741+Armira5@users.noreply.github.com>
2023-08-31 19:06:53 +0200

    creating billboards entity

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -0,0 +1,14 @@
+import { BillboardClient } from "@/app/(dashboard)/[storeId]/(routes)/billboards/components/client"
+
+const BillboardsPage = () => {
+    return (
+    <div className="flex-col1">
+        <div className="flex-1 space-y-4 p-8 pt-6">
+            <BillboardClient />
+        </div>
+        
+    </div>
+    );
+}
+
+export default BillboardsPage;
